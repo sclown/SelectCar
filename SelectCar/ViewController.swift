bfe91db453e88afe8c97887dbe3c19c5103ad945
Dmitriy Kurkin <dm-kurkin@yandex-team.ru>
2019-01-15 17:49:53 +0300

    Initial Commit

---
+++ SelectCar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SelectCar
+//
+//  Created by Dmitry Kurkin on 15/01/2019.
+//  Copyright © 2019 Dmitry Kurkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
